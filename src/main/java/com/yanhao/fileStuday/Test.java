2269e302f4642b1687725fec5d2e6a797686decb
githubyh2 <githubyh2@github.com>
2023-02-20 22:42:39 +0800

    修改Test

--- src/main/java/com/yanhao/fileStuday/Test.java
+++ src/main/java/com/yanhao/fileStuday/Test.java
@@ -3,7 +3,5 @@ package com.yanhao.fileStuday;
 public class Test {
     public static void main(String[] args) {
         System.out.println("aaa");
-        //线程注解
-        new Thread().start();
     }
 }
