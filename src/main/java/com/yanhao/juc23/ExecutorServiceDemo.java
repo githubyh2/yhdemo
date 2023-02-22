60b78b46332beee98e4bf8c78f53a656394e5d3f
githubyh2 <githubyh2@github.com>
2023-02-22 15:19:26 +0800

    新增juc中使用Executors进行创建线程池、自定义使用ThreadPoolExecutor创建线程池

--- src/main/java/com/yanhao/juc23/ExecutorServiceDemo.java
+++ src/main/java/com/yanhao/juc23/ExecutorServiceDemo.java
@@ -1,13 +1,47 @@
 package com.yanhao.juc23;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 /**
  * 通过 Executors 创建的线程池
  */
 public class ExecutorServiceDemo {
     public static void main(String[] args) {
+
+        System.out.println("逻辑处理器数:" + Runtime.getRuntime().availableProcessors());
+
+        final Integer corePoolSize = 2;
+        final Integer maximumPoolSize = 5;
+        final Long keepAliveTime = 1L;
+
+        ExecutorService executorService = new ThreadPoolExecutor(corePoolSize,
+                maximumPoolSize,
+                keepAliveTime,
+                TimeUnit.SECONDS,
+                new LinkedBlockingQueue<>(3),
+                Executors.defaultThreadFactory(),
+                /**
+                 * 线程池的拒绝策略有4种：
+                 * ThreadPoolExecutor.AbortPolicy()默认拒绝策略【触发条件是，请求的线程大于 阻塞队列大小 + 最大线程数】：java.util.concurrent.RejectedExecutionException 直接阻止系统正常运行
+                 * CallerRunsPolicy():"调用者运行"一种调节机制，该策略既不会抛弃任务，也不会抛出异常，而是将某些任务回退到调用者，从而降低新任务的流量
+                 * DiscardOldestPolicy():抛弃队列种等待最久的任务，然后把当前任务加入到队列种，尝试再次提交当前任务
+                 * DiscardPolicy():该策略默默的丢弃无法处理的任务，不予任何处理也不抛异常。如果允许任务丢失，这是最好的一种策略
+                 */
+                new ThreadPoolExecutor.AbortPolicy());
+
+
+        for (int i = 1; i <= 8; i++) {
+            executorService.execute(() -> {
+                System.out.println(Thread.currentThread().getName() + "\t 办理业务");
+            });
+        }
+
+        executorService.shutdown();
+
+
+    }
+
+    private static void initPool() {
         //newFixedThreadPool(3);创建⼀个固定⼤⼩的线程池，可控制并发的线程数，超出的线程会在队列中等待；
 //        ExecutorService executorService = Executors.newFixedThreadPool(3);
 
@@ -29,6 +63,5 @@ public class ExecutorServiceDemo {
             });
         }
         executorService.shutdown();
-
     }
 }
