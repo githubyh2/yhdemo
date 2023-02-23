a0824b60f479ebb26c8ad41980afedf5a931c002
githubyh2 <githubyh2@github.com>
2023-02-23 10:24:43 +0800

    test parseInt

--- src/main/java/com/yanhao/fileStuday/Test.java
+++ src/main/java/com/yanhao/fileStuday/Test.java
@@ -4,5 +4,10 @@ public class Test {
     public static void main(String[] args) {
         //测试
         System.out.println("aaa");
+
+        //Integer中parseInt 使用
+        System.out.println(Integer.parseInt("100"));
+
+
     }
 }
