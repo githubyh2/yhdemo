b94d4326de988f3b086b66e061c7a4908570f0b9
githubyh2 <githubyh2@github.com>
2024-02-22 11:13:50 +0800

    master中unicode转中文字符串

--- src/main/java/com/yanhao/util/CreateTableParser.java
+++ src/main/java/com/yanhao/util/CreateTableParser.java
@@ -6,19 +6,12 @@ import java.util.regex.Pattern;
 /**
  * @author yanhao
  * @data 2024/02/05
+ * unicode转中文字符串
  */
 public class CreateTableParser {
     public static void main(String[] args) {
         String createTableStatement = "CREATE TABLE hive3.ods.s002_d_chnl_da (                                       \n" +
-                "    chnl_id varchar COMMENT U&'\\6E20\\9053ID',                                  \n" +
-                "    chnl_name varchar COMMENT U&'\\6E20\\9053\\540D\\79F0',                        \n" +
-                "    chnl_cat varchar COMMENT U&'\\6E20\\9053\\7C7B\\578B',                         \n" +
-                "    chnl_cls varchar COMMENT U&'\\6E20\\9053\\5206\\7C7B',                         \n" +
-                "    chnl_desc varchar COMMENT U&'\\6E20\\9053\\5206\\7C7B\\63CF\\8FF0',              \n" +
-                "    chnl_name_or varchar COMMENT U&'\\6E20\\9053\\540D\\79F0\\8865\\5145\\8BF4\\660E', \n" +
-                "    chnl_cls_name varchar COMMENT '',                                          \n" +
-                "    circ_cls varchar COMMENT '',                                               \n" +
-                "    chnl_cls_kh varchar COMMENT ''                                             \n" +
+                "    chnl_name varchar COMMENT U&'A\\FF1A\\6E202\\FF1A\\9053\\540D\\FF08\\79F0\\FF09'                        \n" +
                 " )                                                                             \n" +
                 " COMMENT U&'\\8BB0\\5F55\\6E20\\9053\\4FE1\\606F'                                    \n" +
                 " WITH (                                                                        \n" +
@@ -73,20 +66,55 @@ public class CreateTableParser {
     private static String convertUnicodeSequenceToChinese(String unicodeSequence) {
         StringBuilder result = new StringBuilder();
         String[] hexValues = unicodeSequence.split("\\\\");
-        for (String hex : hexValues) {
+        for (int i = 0; i < hexValues.length; i++) {
+            String hex = hexValues[i];
             if (!hex.isEmpty()) {
                 if (hex.startsWith("U&")) {
                     hex = hex.substring(2);  // 去掉开头的U&
                 }
-                if (hex.length() == 4) {
-                    int unicodeValue = Integer.parseInt(hex, 16);
-                    result.append((char) unicodeValue);
-                } else {
-                    int unicodeValue = Integer.parseInt(hex.substring(0, 4), 16);
-                    result.append((char) unicodeValue + hex.substring(4));
+
+                if (i == 0) {
+                    result.append(hex);
+                } else if (i > 0) {
+                    if (hex.length() == 4) {
+                        int unicodeValue = Integer.parseInt(hex, 16);
+                        result.append((char) unicodeValue);
+                    } else {
+                        int unicodeValue = Integer.parseInt(hex.substring(0, 4), 16);
+                        result.append((char) unicodeValue + hex.substring(4));
+                    }
                 }
             }
         }
+
+//        for (String hex : hexValues) {
+//            if (!hex.isEmpty()) {
+//                if (hex.startsWith("U&")) {
+//                    hex = hex.substring(2);  // 去掉开头的U&
+//                }
+//                // Character.isAlphabetic(hex.charAt(0))
+//                //  [a-zA-Z].*
+//
+////                if (Character.isAlphabetic(hex.charAt(0)) || hex.matches("^[_\\-\\&\\W].*")
+////                        || (hex.trim().length() < 4 && hex.matches("^\\d+.*"))) {
+////                    result.append(hex);
+////                } else if (hex.length() == 4) {
+////                    int unicodeValue = Integer.parseInt(hex, 16);
+////                    result.append((char) unicodeValue);
+////                } else if (hex.length() > 4) {
+////                    int unicodeValue = Integer.parseInt(hex.substring(0, 4), 16);
+////                    result.append((char) unicodeValue + hex.substring(4));
+////                }
+//
+//                if (hex.length() == 4) {
+//                    int unicodeValue = Integer.parseInt(hex, 16);
+//                    result.append((char) unicodeValue);
+//                } else {
+//                    int unicodeValue = Integer.parseInt(hex.substring(0, 4), 16);
+//                    result.append((char) unicodeValue + hex.substring(4));
+//                }
+//            }
+//        }
         return result.toString();
     }
 }
